8303f8b5f18239297c401e1e87b3da8fe5c2eba2
Dương Ngọc Quân <Ramayana1202@users.noreply.github.com>
2017-08-21 09:27:44 +0700

    Initial commit

---
+++ src/providers/providers.ts
@@ -0,0 +1,16 @@
+import { Login } from './login';
+import { Api } from './api';
+import { Settings } from './settings';
+import { ReportComplete } from './report-complete';
+import { InventoryTransfers } from './inventory-transfers';
+import { Common, MsgType } from './common';
+
+export {
+    Api,
+    Login,
+    ReportComplete,
+    InventoryTransfers,
+    Common,
+    MsgType,
+    Settings
+};
